68e53686d8c3b0cbde8e833615fa68d19466e02c
MD. TAIFUR RAHMAN JASIM <taif.jnu@gmail.com>
2026-09-23 00:35:31 +0600

    refactor: separate note data

---
+++ data/notes.py
@@ -0,0 +1,14 @@
+notes = [
+    {
+        "id": 1,
+        "title": "Learn Python",
+        "content": "Python is the foundation of NoteHub.",
+        "author": "Taifur",
+    },
+    {
+        "id": 2,
+        "title": "Learn FastAPI",
+        "content": "FastAPI will be used to build the NoteHub API.",
+        "author": "Taifur",
+    },
+]
--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-notes = [
-    {
-        "id": 1,
-        "title": "Learn Python",
-        "content": "Python is the foundation of NoteHub.",
-        "author": "Taifur",
-    },
-    {
-        "id": 2,
-        "title": "Learn FastAPI",
-        "content": "FastAPI will be used to build the NoteHub API.",
-        "author": "Taifur",
-    },
-]
+from data.notes import notes
 
 def get_notes():
     return notes

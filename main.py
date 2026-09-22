2d182c512016770d428629eefe3c94e5092a4f47
MD. TAIFUR RAHMAN JASIM <taif.jnu@gmail.com>
2026-09-23 00:22:11 +0600

    feat: add notes data and get notes function

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello, NoteHub!")
+notes = [
+    {
+        "id": 1,
+        "title": "Learn Python",
+        "content": "Python is the foundation of NoteHub.",
+        "author": "Taifur",
+    },
+    {
+        "id": 2,
+        "title": "Learn FastAPI",
+        "content": "FastAPI will be used to build the NoteHub API.",
+        "author": "Taifur",
+    },
+]
+
+def get_notes():
+    return notes
+
+
+print(get_notes())
